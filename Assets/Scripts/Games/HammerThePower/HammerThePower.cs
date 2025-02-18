2472e71bce4be79c0c1ced5f88ad557714d4edb4
Ilay Sh <ilayshlomo@gmail.com>
2025-02-18 14:53:44 +0200

    added some more to hammer the power

--- Assets/Scripts/Games/HammerThePower/HammerThePower.cs
+++ Assets/Scripts/Games/HammerThePower/HammerThePower.cs
@@ -1,17 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HammerThePower : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private Image hintImage;
 
-    // Update is called once per frame
-    void Update()
+    void Start()
     {
         
     }
