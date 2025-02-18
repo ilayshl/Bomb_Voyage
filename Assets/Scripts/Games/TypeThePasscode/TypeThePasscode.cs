094ea68a215860a8474c3c6a6e9b696e77f403a6
Ilay Sh <ilayshlomo@gmail.com>
2025-02-18 11:06:13 +0200

    fixed buttons on Type The Passcode
    - added buttons and sprites, still need to add the hidden passoword on the bomb screen and fix 7-< buttons hitboxes.

--- Assets/Scripts/Games/TypeThePasscode/TypeThePasscode.cs
+++ Assets/Scripts/Games/TypeThePasscode/TypeThePasscode.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 public class TypeThePasscode : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI text;
-    private int passcode;
+    private int passcode; //The passcode generated. this may not change once set.
     private int typedPasscode;
 
     private GameManager _gameManager;
@@ -22,12 +22,20 @@ public class TypeThePasscode : MonoBehaviour
         text.SetText(passcode.ToString());
     }
 
+    /// <summary>
+    /// Generates a radom 6 digits passcode.
+    /// </summary>
+    /// <returns></returns>
     private int GeneratePasscode()
     {
         int random = Random.Range(100000, 999999);
         return random;
     }
 
+    /// <summary>
+    /// If the game isn't lost already, adds the given digit to the typed passcode.
+    /// </summary>
+    /// <param name="number"></param>
     public void PressKeypad(int number)
     {
         if(!gameLost)
@@ -48,12 +56,18 @@ public class TypeThePasscode : MonoBehaviour
 
     }
 
+    /// <summary>
+    /// Deletes the last digit from the typed passcode.
+    /// </summary>
     public void DeleteDigit()
     {
         float temporaryPasscode = typedPasscode/10;
         typedPasscode = (int)temporaryPasscode;
     }
 
+    /// <summary>
+    /// When the game is won.
+    /// </summary>
     public void OnWin()
     {
         _gameManager.OnWin();
