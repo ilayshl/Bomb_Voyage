a1307433496cf36a3ce0e93124e2d2c0a35160c0
Ilay Sh <ilayshlomo@gmail.com>
2025-02-19 19:46:58 +0200

    added Hammer The Power
    - added Hammer The Power - fully works
    - still needs to add sprites for the manual in hammer the power

--- Assets/Scripts/Games/HammerThePower/HammerThePower.cs
+++ Assets/Scripts/Games/HammerThePower/HammerThePower.cs
@@ -1,12 +1,85 @@
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class HammerThePower : MonoBehaviour
 {
     [SerializeField] private Image hintImage;
+    [SerializeField] private Sprite[] hints;
+    [SerializeField] private PowerSupply[] _supplyUp;
+    [SerializeField] private PowerSupply[] _supplyRight;
+    [SerializeField] private PowerSupply[] _supplyDown;
+    [SerializeField] private PowerSupply[] _supplyLeft;
 
-    void Start()
+    int randomDirection;
+
+    private List<PowerSupply> _connectedSupplies = new List<PowerSupply>();
+
+    private GameManager _gameManager;
+
+    private void Awake()
     {
-        
+        _gameManager = FindObjectOfType<GameManager>();
+        RectTransform rectTransform = GetComponent<RectTransform>();
+        rectTransform.anchoredPosition -= new Vector2(70, rectTransform.anchoredPosition.y);
+    }
+
+    private void Start()
+    {
+        SetConnectedPowerSupplies(_supplyUp);
+        SetConnectedPowerSupplies(_supplyRight);
+        SetConnectedPowerSupplies(_supplyDown);
+        SetConnectedPowerSupplies(_supplyLeft);
+
+        SetActivePowerSupply();
+    }
+
+    private void DecideOnDirection()
+    {
+        randomDirection = Random.Range(0, _connectedSupplies.Count);
+        hintImage.sprite=hints[randomDirection];
+    }
+
+    private void SetConnectedPowerSupplies(PowerSupply[] supplies)
+    {
+        int randomIndex = Random.Range(0, supplies.Length);
+        supplies[randomIndex].SetConnected();
+        _connectedSupplies.Add(supplies[randomIndex]);
+    }
+
+    /// <summary>
+    /// Sets the active power supply that's meant to be broken.
+    /// </summary>
+    private void SetActivePowerSupply()
+    {
+        DecideOnDirection();
+        _connectedSupplies.ElementAt(randomDirection).SetActiveSupply();
+    }
+
+    /// <summary>
+    /// Checks if the power broken is the correct one.
+    /// </summary>
+    /// <param name="isConnected"></param>
+    /// <param name="isActive"></param>
+    public void OnPowerBreak(bool isConnected, bool isActive)
+    {
+        //Play Sound
+        if(isConnected && isActive)
+        {
+            GetComponent<Animator>().SetTrigger("gameWon");
+        }
+        if(isConnected && !isActive)
+        {
+            _gameManager.OnLose();
+        }
+    }
+
+    /// <summary>
+    /// Finishes the game- gets called from the animation.
+    /// </summary>
+    public void OnWin()
+    {
+        _gameManager.OnWin();
     }
 }
